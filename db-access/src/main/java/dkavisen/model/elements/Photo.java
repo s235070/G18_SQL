8a1359ef6b4a933c1333d92c7dc516850ce90371
Andrei Enache s235089 <s235089@student.dtu.dk>
2024-03-19 09:36:59 +0100

    Record photo brought in line with class

--- db-access/src/main/java/dkavisen/model/elements/Photo.java
+++ db-access/src/main/java/dkavisen/model/elements/Photo.java
@@ -1,8 +1,17 @@
 package dkavisen.model.elements;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public record Photo (
     String title,
     Date date
-) {}
+) {
+    @Override
+    public String toString() {
+        final String D = ";";
+        final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyyMMdd");
+
+        return title +D + dateFormatter.format(date);
+    }
+}
