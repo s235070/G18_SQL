fc98fa3abb7727375f759008adc26f99c79c384a
Andrei Enache s235089 <s235089@student.dtu.dk>
2024-03-19 08:11:24 +0100

    Empty PhotoReporterUploader added

---
+++ db-access/src/main/java/dkavisen/uploader/PhotoReporterUploader.java
@@ -0,0 +1,9 @@
+package dkavisen.uploader;
+
+import dkavisen.model.relations.PhotoReporter;
+
+public class PhotoReporterUploader {
+    public void upload(PhotoReporter photoReporter) {
+
+    }
+}
