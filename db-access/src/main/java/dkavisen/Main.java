74d06f57ffa99c47fcfd49dc30f9ef0a2a8cc757
Andrei Enache <andreienake@gmail.com>
2024-03-28 01:32:29 +0100

    Untested IO changes

--- db-access/src/main/java/dkavisen/Main.java
+++ db-access/src/main/java/dkavisen/Main.java
@@ -1,26 +1,39 @@
 package dkavisen;
 
+import dkavisen.input.output.DatabaseQueryExecutor;
 import dkavisen.loader.PhotoReporterLoader;
 import dkavisen.model.relations.PhotoReporter;
 import dkavisen.uploader.PhotoReporterUploader;
-import dkavisen.model.elements.Shoots;
-import dkavisen.model.elements.Photo;
-import dkavisen.model.elements.Reporter;
 
 import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         PhotoReporterLoader loader = new PhotoReporterLoader();
         PhotoReporterUploader uploader = new PhotoReporterUploader();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Which file do you want to upload from?");
         try {
-            ArrayList<PhotoReporter> list = loader.from("uploads.csv");
+            ArrayList<PhotoReporter> list = loader.from(scanner.nextLine() + ".csv");
             list.stream().forEach(uploader::upload);
+            System.out.println("Upload successfully completed");
         } catch (FileNotFoundException | ParseException e) {
-            e.printStackTrace();
+            System.out.println("Something went wrong! " + e.getMessage());
         }
+
+        System.out.println("Now running SELECT queries, name the table to be queried");
+        System.out.println("Exit by writing \"exit\"");
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.equalsIgnoreCase("exit")) break;
+            else DatabaseQueryExecutor.printQuery(line);
+        }
+
+        System.out.println("Now exiting program...");
+        scanner.close();
     }
 }
--- db-access/src/main/java/dkavisen/input/output/DatabaseQueryExecutor.java
+++ db-access/src/main/java/dkavisen/input/output/DatabaseQueryExecutor.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class DatabaseQueryExecutor {
-    public void printQuery(){
+    public static void printQuery(String table){
         final String dbURL = "jdbc:mysql://localhost:3306/project";
         final String user = "root";
         final String password = "mypassword";
@@ -13,51 +13,39 @@ public class DatabaseQueryExecutor {
             Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             Scanner scanner = new Scanner(System.in);
         ) {
-            System.out.println("\n*** Connected to the database. ***");
+            System.out.println();
 
-            String sqlQuery;
-            while (true) {
-                System.out.println("\nEnter a SELECT SQL query or type 'exit' to quit:");
-                sqlQuery = scanner.nextLine();
+            try {
+                ResultSet rs = stmt.executeQuery("SELECT * FROM " + table + ";");
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnsNumber = rsmd.getColumnCount();
+                int[] columnWidths = new int[columnsNumber];
+
+                for (int i = 1; i <= columnsNumber; i++) {
+                    columnWidths[i - 1] = rsmd.getColumnName(i).length();
+                    }
+                while (rs.next()) {
+                    for (int i = 1; i <= columnsNumber; i++) {
+                        columnWidths[i - 1] = Math.max(columnWidths[i - 1], rs.getString(i) != null ? rs.getString(i).length() : 0);
+                    }
+                }
+
+                rs.beforeFirst();
+
+                for (int i = 1; i <= columnsNumber; i++) {
+                    System.out.print(String.format("%-" + columnWidths[i - 1] + "s  ", rsmd.getColumnName(i)));
+                }
                 System.out.println();
 
-                if ("exit".equalsIgnoreCase(sqlQuery)) {
-                    System.out.println("Exiting...");
-                    break;
-                }
-
-                try {
-                    ResultSet rs = stmt.executeQuery(sqlQuery);
-                    ResultSetMetaData rsmd = rs.getMetaData();
-                    int columnsNumber = rsmd.getColumnCount();
-                    int[] columnWidths = new int[columnsNumber];
-
+                while (rs.next()) {
                     for (int i = 1; i <= columnsNumber; i++) {
-                        columnWidths[i - 1] = rsmd.getColumnName(i).length();
-                    }
-                    while (rs.next()) {
-                        for (int i = 1; i <= columnsNumber; i++) {
-                            columnWidths[i - 1] = Math.max(columnWidths[i - 1], rs.getString(i) != null ? rs.getString(i).length() : 0);
-                        }
-                    }
-
-                    rs.beforeFirst();
-
-                    for (int i = 1; i <= columnsNumber; i++) {
-                        System.out.print(String.format("%-" + columnWidths[i - 1] + "s  ", rsmd.getColumnName(i)));
+                        String columnValue = rs.getString(i) == null ? "NULL" : rs.getString(i);
+                        System.out.print(String.format("%-" + columnWidths[i - 1] + "s  ", columnValue));
                     }
                     System.out.println();
-
-                    while (rs.next()) {
-                        for (int i = 1; i <= columnsNumber; i++) {
-                            String columnValue = rs.getString(i) == null ? "NULL" : rs.getString(i);
-                            System.out.print(String.format("%-" + columnWidths[i - 1] + "s  ", columnValue));
-                        }
-                        System.out.println();
-                    }
-                } catch (SQLException e) {
-                    System.out.println("Error executing query: " + e.getMessage());
                 }
+            } catch (SQLException e) {
+                System.out.println("Error executing query: " + e.getMessage());
             }
         } catch (SQLException e) {
             e.printStackTrace();
