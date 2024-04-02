5db722233e771a5a34805c0407fe877af166befb
Daniall <s235070@dtu.dk>
2024-04-02 11:38:27 +0200

    yes

---
+++ db-access/src/test/java/dkavisen/loader/PhotoReporterLoaderTest.java
@@ -0,0 +1,22 @@
+package dkavisen.loader;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/*import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import dkavisen.model.elements.Photo;
+import dkavisen.model.elements.Reporter;
+import dkavisen.model.elements.Shoots;
+import dkavisen.model.relations.PhotoReporter; */
+
+public class PhotoReporterLoaderTest {
+
+    
+}
